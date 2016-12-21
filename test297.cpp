0a3854ace6cfb685eee4d8212240c03c406c26d3
Ericwangjiaxiang <332395526@qq.com>
2016-12-21 17:33:13 +0800

    第二次修改
    改了一些东西

--- test297.cpp
+++ test297.cpp
@@ -1,2 +1,12 @@
 #include<cstdio>
-#include<ct> 
+#include<cstring>
+#include<queue>
+#include<algorithm>
+using namespace std;
+const int maxn = 50;
+struct node{
+	int data;
+	node* lchild;
+	node* rchild;
+}; 
+int pre[maxn], in[maxn], post[maxn]
