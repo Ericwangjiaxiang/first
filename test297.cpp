13fc19ee5cb6dfc962cf585c23cc4945d399861d
Ericwangjiaxiang <332395526@qq.com>
2016-12-21 20:39:34 +0800

    层序遍历的实现
    算法的思想为：
    给出后续遍历和中序遍历。这样的话，通过两个数组记录先序遍历和中序遍历；通过这样的话可以唯一的确定二叉树，建树。
    
    然后通过BFS的思想，通过建立的树，进行广度优先搜索。首先定义一个指向结构的指针队列，将根节点先入栈、然后进入循环体，当队列不为空时，然后定义一个变量指向队首元素，将队首元素出栈然后访问出栈的元素。

--- test297.cpp
+++ test297.cpp
@@ -9,4 +9,61 @@ struct node{
 	node* lchild;
 	node* rchild;
 }; 
+int pre[maxn], in[maxn], post[maxn];//先序、中序、后序
+int n;//结点个数
+
+//当前二叉树的后续序列曲线为[postL, postR]，中序序列区间为[inL, inR]
+//creat函数返回构建出的二叉树的根节点地址
+node* create(int postL, int postR, int inL, int inR){
+	if(postL > postR){
+		return NULL;//后序序列长度小于等于0，直接返回 
+	}
+	node* root = new node;//新建一个新的节点，用来存放当前二叉树的根节点
+	root->data = post[postR];//新节点的数据域为根节点的值
+	int k;
+	for(k = inL; k <= inR; k++){
+		if(in[k] == post[postR]){//在中序序列中找到in[k] == pre[L]的节点 
+			break;
+		}
+	} 
+	int numLeft = k - inL;//左子树的节点的个数
+	//返回左子树的根节点地址，赋值给root的左指针
+	root->lchild = create(postL, postL + numLeft - 1, inL, k - 1);
+	//返回右子树的根节点地址，赋值给root的右指针
+	root->rchild = create(postL + numLeft, postR - 1, k + 1, inR);
+	return root; //返回根节点地址 
+} 
+int num = 0; //已输出节点个数 
+void BFS(node* root){
+	queue<node*> q;//注意队列里是存地址
+	q.push(root);//将根节点地址入队
+	while(!q.empty()){
+		node* now = q.front();//取出队首元素
+		q.pop();
+		printf("%d",now->data);//访问队首元素 
+		num++;
+		if(num < n){
+			printf(" ");
+		} 
+		if(now->lchild != NULL){//左子树非空 
+			q.push(now->lchild); 
+		}
+		if(now->rchild != NULL){//右子树非空 
+			q.push(now->rchild);
+		}
+		
+	} 
+} 
+
+int main(void){
+	scanf("%d", &n);
+	for(int i = 0; i < n; i++){
+		scanf("%d", &post[i]);
+	}
+	for(int i = 0; i < n; i++){
+		scanf("%d", &in[i]);
+	}
+	node* root = create(0, n - 1, 0, n -1); //建树
+	BFS(root);   //层序遍历 
+} 
 
